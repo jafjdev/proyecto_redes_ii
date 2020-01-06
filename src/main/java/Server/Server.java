bba2d370e72b4b13ab1f8b233ea44175503fd29b
jafjdev <jafjdev@gmail.com>
2020-01-06 15:00:52 -0400

    [Server] Creating Folders

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -1,17 +1,25 @@
 package Server;
 
 import Paquete.PackageToReceive;
+import Server.Exceptions.FolderAlreadyExistException;
+import Server.Files.FileDAO;
+import Server.Files.IFileManager;
+import Server.User.IUserManager;
+import Server.User.UserDAO;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server implements Runnable {
-    private PackageToReceive _package;
+    IUserManager userManager;
+    IFileManager fileManager;
 
     public Server() {
         Thread thread = new Thread(this);
         thread.start();
+        userManager = new UserDAO();
+        fileManager = new FileDAO();
     }
 
     @Override
@@ -20,15 +28,25 @@ public class Server implements Runnable {
             ServerSocket serverSocket = new ServerSocket(9999);
             while (true) {
                 Socket socket = serverSocket.accept();
+                //////////////////////////////////////////////////////////////////
+                /////                   INPUT                              /////
+                //////////////////////////////////////////////////////////////////
                 ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
-                this._package = (PackageToReceive) objectInputStream.readObject();
+                PackageToReceive _packageToReceive = (PackageToReceive) objectInputStream.readObject();
+                if (_packageToReceive.get_code() == 0)
+                    fileManager.createFolder(_packageToReceive.get_folderName());
 
+                //////////////////////////////////////////////////////////////////
+                /////                   OUTPUT                              /////
+                //////////////////////////////////////////////////////////////////
                 ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
 
                 socket.close();
             }
         } catch (IOException | ClassNotFoundException e) {
             System.out.println(e.getMessage());
+        } catch (FolderAlreadyExistException e) {
+            e.printStackTrace();
         }
 
     }
--- src/main/java/Server/main.java
+++ src/main/java/Server/main.java
@@ -1,17 +1,7 @@
 package Server;
 
-import Server.Config.Registry;
-import Server.Exceptions.FolderAlreadyExistException;
-import Server.Files.FileDAO;
-import Server.Files.IFileManager;
 public class main {
     public static void main(String[] args) {
-        Registry registry = Registry.getInstance();
-        IFileManager fileManager = new FileDAO();
-        try {
-            fileManager.createFolder("Pepito");
-        } catch (FolderAlreadyExistException e) {
-            e.printStackTrace();
-        }
+        Server server = new Server();
     }
 }
